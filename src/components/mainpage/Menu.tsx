c8047e928b037cf46c932cb18bdd8eb12b6c1bf5
Zohir <vasv29834@gmail.com>
2023-11-03 00:59:20 +0500

    Some designed

--- src/components/mainpage/Menu.tsx
+++ src/components/mainpage/Menu.tsx
@@ -8,10 +8,10 @@ function Menu({isClicked}) {
   return (
     <div className={`w-full absolute bottom-0 z-0 delay-100 transparent transition-all overflow-hidden ease-in-out left-0 ${isClicked ? 'h-screen' : 'h-0'}`}>
         <div className="bg absolute w-full h-full flex justify-between">
-          <div className={`bg-side flex justify-center items-center bottom-0 absolute w-6/12 left-0 transition-all delay-200 bg-fuchsia-900 ${isClicked ? 'h-screen' : 'h-0'} bg-ima`}>
+          <div className={`bg-side flex justify-center items-center bottom-0 absolute w-full left-0 transition-all delay-200 lg:w-6/12 bg-fuchsia-900 ${isClicked ? 'h-screen' : 'h-0'}`}>
             <img src={jpBg} className='opacity-50 w-4/5' alt="" />
           </div>
-          <div className={`bg-side bottom-0 absolute w-6/12 overflow-hidden right-0 transition-all delay-300 bg-black  ${isClicked ? 'h-screen' : 'h-0'}`}>
+          <div className={`bg-side bottom-0 absolute w-0 overflow-hidden right-0 transition-all delay-300 bg-black lg:w-6/12 ${isClicked ? 'h-screen' : 'h-0'}`}>
             <img className='w-0 h-auto absolute bottom-2 right-4 lg:w-3/5' src={jpPh} />
             <img className=' w-full h-full top-14 left-0 absolute lg:w-3/5 lg:top-16 lg:h-auto lg:left-4 ' src={jpPh1} />
           </div>
