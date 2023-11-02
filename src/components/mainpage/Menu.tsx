429429fb1e73122cdcf906e4d2b048ae7d1f4502
Zohir <vasv29834@gmail.com>
2023-11-03 00:51:20 +0500

    Menu-animation

--- src/components/mainpage/Menu.tsx
+++ src/components/mainpage/Menu.tsx
@@ -1,8 +1,52 @@
 
+import jpPh from '../../img/jp.jpg';
+import jpPh1 from '../../img/jp2.jpg';
+import jpBg from '../../img/bg-jp.svg'
+
+
 function Menu({isClicked}) {
   return (
-    <div className={`w-full absolute bottom-0 z-0 delay-100 bg-red-400 transition-all ease-in-out left-0 ${isClicked ? 'h-screen  pt-14 ' : 'h-0'}`}>
-      Menu
+    <div className={`w-full absolute bottom-0 z-0 delay-100 transparent transition-all overflow-hidden ease-in-out left-0 ${isClicked ? 'h-screen' : 'h-0'}`}>
+        <div className="bg absolute w-full h-full flex justify-between">
+          <div className={`bg-side flex justify-center items-center bottom-0 absolute w-6/12 left-0 transition-all delay-200 bg-fuchsia-900 ${isClicked ? 'h-screen' : 'h-0'} bg-ima`}>
+            <img src={jpBg} className='opacity-50 w-4/5' alt="" />
+          </div>
+          <div className={`bg-side bottom-0 absolute w-6/12 overflow-hidden right-0 transition-all delay-300 bg-black  ${isClicked ? 'h-screen' : 'h-0'}`}>
+            <img className='w-0 h-auto absolute bottom-2 right-4 lg:w-3/5' src={jpPh} />
+            <img className=' w-full h-full top-14 left-0 absolute lg:w-3/5 lg:top-16 lg:h-auto lg:left-4 ' src={jpPh1} />
+          </div>
+
+          <div className="menu-content-wrapper w-full pt-14 z-10 flex justify-center">
+              <div className="menu-content w-11/12 flex justify-self-start flex-col items-start p-10">
+
+                  <p className=" text-2xl h-10 z-10 font-extrabold text-white relative cursor-pointer group flex justify-center p-1">
+                    Японский язык - это просто
+                    <p className="w-full group-hover:top-0  group-hover:left-0 bg-black -z-10 text-zinc-800 transition-all delay-75 absolute top-1.5 left-2.5 p-1">Японский язык - это просто</p>
+                  </p>
+
+                  <p className="mt-4 ml-4 text-xl h-10 z-10 font-extrabold text-white relative cursor-pointer group flex justify-center p-1">
+                    Страница тестов
+                    <p className="w-full group-hover:top-0  group-hover:left-0 bg-black -z-10 text-zinc-800 transition-all delay-75 absolute top-1.5 left-2.5 p-1">Страница тестов</p>
+                  </p>
+                  <p className="ml-4 mt-2 text-xl h-10 z-10 font-extrabold text-white relative cursor-pointer group flex justify-center p-1">
+                    Наши уроки
+                    <p className="w-full group-hover:top-0  group-hover:left-0 bg-black -z-10 text-zinc-800 transition-all delay-75 absolute top-1.5 left-2.5 p-1">Наши уроки</p>
+                  </p>
+                  <p className="ml-4 mt-2 text-xl h-10 z-10 font-extrabold text-white relative cursor-pointer group flex justify-center p-1">
+                    Рекомендации
+                    <p className="w-full group-hover:top-0  group-hover:left-0 bg-black -z-10 text-zinc-800 transition-all delay-75 absolute top-1.5 left-2.5 p-1">Рекомендации</p>
+                  </p>
+                  <p className="ml-4 mt-2 text-xl h-10 z-10 font-extrabold text-white relative cursor-pointer group flex justify-center p-1">
+                    Обновления сайта
+                    <p className="w-full group-hover:top-0  group-hover:left-0 bg-black -z-10 text-zinc-800 transition-all delay-75 absolute top-1.5 left-2.5 p-1">Обновления сайта</p>
+                  </p>
+                  <p className="ml-4 mt-2 text-xl h-10 z-10 font-extrabold text-white relative cursor-pointer group flex justify-center p-1">
+                    О нас
+                    <p className="w-full group-hover:top-0  group-hover:left-0 bg-black -z-10 text-zinc-800 transition-all delay-75 absolute top-1.5 left-2.5 p-1">О нас</p>
+                  </p>
+              </div>
+          </div>
+        </div>
     </div>
   )
 }
