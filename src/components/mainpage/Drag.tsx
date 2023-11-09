8449897889d12050f66dfaf7c7f2c739d91e4dd7
Zohir <vasv29834@gmail.com>
2023-11-09 14:28:06 +0500

    Updated

--- src/components/mainpage/Drag.tsx
+++ src/components/mainpage/Drag.tsx
@@ -3,8 +3,12 @@ import { Parallax } from 'react-scroll-parallax'
 
 function Drag() {
 
+  // const containerStyle = {
+  //   background: 'radial-gradient(circle, rgb(180, 30, 206),rgb(126, 34, 206))',
+  // };
+
   return (
-    <div className="w-full h-48 sm:h-96 overflow-hidden flex items-center relative bg-purple-700">
+    <div className="w-full h-48 sm:h-96 overflow-hidden flex items-center relative bg-gray-800">
         <div className="bg z-0 w-56 h-56 bottom-0 left-0 lg:left-20 absolute">
           <img src={saitama} className='w-36 h-auto absolute -bottom-3'/>
           <div className="text-saitama rounded-3xl shadow-slate-700 shadow-md w-auto absolute bottom-32 left-32 text-xs h-auto p-2 break-words bg-white ">Добро пожаловать</div>
